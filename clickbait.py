3b35e8473266242c8f359fffa11ef50f4c56ed04
Lucino <lrchiafullo@brandeis.edu>
2018-12-09 12:38:25 -0500

    added small code for news api object and my own api key

--- clickbait.py
+++ clickbait.py
@@ -4,3 +4,9 @@ File to fill with fun clickbait analysis!
 Love, Lucino
 
 """
+
+from newsapi import NewsApiClient
+
+news_api = NewsApiClient(api_key='063f02817dbb49528058d7372964f645')
+sources = news_api.get_sources()
+print(sources)
