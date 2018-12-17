753fbfde48341d790489eebc8734ed8dd351a6ba
Lucino <lrchiafullo@brandeis.edu>
2018-12-16 19:44:41 -0500

    pickled headlines to reduce api calls and run times.  Generates new bigrams for each training set~

--- clickbait.py
+++ clickbait.py
@@ -3,159 +3,157 @@ File to fill with fun clickbait analysis!
 Love, Lucino
 """
 
-from newsapi import NewsApiClient
 import nltk
 from nltk.tag import pos_tag
 from nltk.tokenize import word_tokenize
 import random
+import pickle
 from collections import Counter
 from nltk.corpus import stopwords
 
+common_bigrams = []
+"""
+Opens the pickled file and shuffles it up, returning a list of labeled headlines
+"""
 
-##not sure where to put this code or how to condense it, but basically this section is where i grab the most 
-#common bigrams from the buzzfeed/mtv news corpus, so we can use them in the bigrams feature later.
-news_api = NewsApiClient(api_key='8c032ece9401408182134442e81ebfc9')
-buzzfeed_headlines = []
-x = 1
-while x <= 10:
-	b_headlines = news_api.get_everything(sources='buzzfeed, mtv-news',from_param='2018-11-16',to='2018-12-15',language='en',sort_by='relevancy',page_size=100,page=x)['articles']
-	b_titles = [article['title'] for article in b_headlines]
-	b_titles = list(filter(None.__ne__,b_titles))
-	buzzfeed_headlines.extend(b_titles)
-	x += 1
-raw = " ".join(buzzfeed_headlines)
-tokens = raw.split(" ")
-bigrams = list(nltk.bigrams(tokens))
-fdist = nltk.FreqDist(bigrams)
-most_common = fdist.most_common(25)
-common_bigrams = [x[0] for x in most_common]
-
-#Pulls two lists of articles from newsAPI, extracting titles only: one consisting of Buzzfeed and MTV News headlines, 
-#bulk-labeled as clickbait, and another consisting of Reuters and Associated Press headlines, bulk-labeled as non-clickbait.
 
 def create_labeled_data():
-	news_api = NewsApiClient(api_key='8c032ece9401408182134442e81ebfc9')
-	buzzfeed_headlines = []
-	reuters_headlines = []
-	x = 1
-	while x <= 10:
-		b_headlines = news_api.get_everything(sources='buzzfeed, mtv-news',from_param='2018-11-16',to='2018-12-15',language='en',sort_by='relevancy',page_size=100,page=x)['articles']
-		r_headlines = news_api.get_everything(sources='reuters, associated-press',from_param='2018-11-16',to='2018-12-15',language='en',sort_by='relevancy',page_size=100,page=x)['articles']
-		b_titles = [article['title'] for article in b_headlines]
-		b_titles = list(filter(None.__ne__,b_titles))
-		b_titles = [(article, 'bait') for article in b_titles]
-		buzzfeed_headlines.extend(b_titles)
-		r_titles = [(article['title'], 'not_bait') for article in r_headlines]
-		reuters_headlines.extend(r_titles)
-		x += 1
-	all_headlines = buzzfeed_headlines + reuters_headlines
-	random.shuffle(all_headlines)
-	return all_headlines
-    
+    all_headlines = pickle.load( open('headlines.p', 'rb') )
+    random.shuffle(all_headlines)
+    return all_headlines
+
 
 def create_feature_sets(labeled_data):
-	featuresets = [(bait_features(headline), label) for (headline, label) in labeled_data]
-	print(len(featuresets))
-	train_set, test_set = featuresets[:1700], featuresets[1700:]
-	return train_set, test_set
+    common = get_bigrams(labeled_data)
+    common_bigrams.extend(common)
+    feature_sets = [(bait_features(headline), label) for (headline, label) in labeled_data]
+    print(len(feature_sets))
+    train_set, test_set = feature_sets[:2700], feature_sets[2700:]
+    return train_set, test_set
+
+
+#pulls out most frequent bigrams from the training set each time (they are almost always the same!)
+def get_bigrams(lst):
+    training = lst[:2700]
+    training = [w for w in training if w[1] == 'bait']
+    training = [w[0] for w in training]
+    raw = " ".join(training)
+    tokens = raw.split(" ")
+    bigrams = list(nltk.bigrams(tokens))
+    fdist = nltk.FreqDist(bigrams)
+    most_common = fdist.most_common(25)
+    common_bigrams = [x[0] for x in most_common]
+    return common_bigrams
+
 
 def bait_features(headline):
-	featureset = {}
-	featureset['procount'] = procount(headline)
-	featureset['punct'] = punct(headline)
-	featureset['averagewordlength'] = averagewordlength(headline)
-	featureset['mostcommontag'] = mostcommontag(headline)
-	featureset['wh'] = wh(headline)
-	featureset['startswithnum'] = startswithnum(headline)
-	featureset['imperative'] = imperative(headline)
-	featureset['bigrams'] = bigrams(headline)
-	return featureset
-	
-#Checks for the use of first and second-person pronouns in article headline; returns true if any found.
+    featureset = {}
+    featureset['procount'] = procount(headline)
+    featureset['punct'] = punct(headline)
+    featureset['averagewordlength'] = averagewordlength(headline)
+    featureset['mostcommontag'] = mostcommontag(headline)
+    featureset['wh'] = wh(headline)
+    featureset['startswithnum'] = startswithnum(headline)
+    featureset['imperative'] = imperative(headline)
+    featureset['bigrams'] = bigrams(headline)
+    return featureset
+
+
+# Checks for the use of first and second-person pronouns in article headline; returns true if any found.
 def procount(headline):
-	pronouns = ["we","you","i","everyone","us","your","our"]
-	for w in word_tokenize(headline):
-		if w.lower() in pronouns:
-			return True
-	return False
+    pronouns = ["we", "you", "i", "everyone", "us", "your", "our"]
+    for w in word_tokenize(headline):
+        if w.lower() in pronouns:
+            return True
+    return False
 
-#Checks end-of-sentence punctuation count in headline; returns true if count is greater than 0.
+
+# Checks end-of-sentence punctuation count in headline; returns true if count is greater than 0.
 def punct(headline):
-	count = 0
-	punct = [".","!","?"]
-	for w in word_tokenize(headline):
-		if w in punct:
-			count += 1
-	return count > 0
+    count = 0
+    punct = [".", "!", "?"]
+    for w in word_tokenize(headline):
+        if w in punct:
+            count += 1
+    return count > 0
 
-#Checks whether the headline starts with a digit; returns true if so.
+
+# Checks whether the headline starts with a digit; returns true if so.
 def startswithnum(headline):
-	tags = [w[1] for w in pos_tag(word_tokenize(headline))]
-	if tags[0] == 'CD':
-		return True
-	return False
+    tags = [w[1] for w in pos_tag(word_tokenize(headline))]
+    if tags[0] == 'CD':
+        return True
+    return False
 
-#Calculates average word length within headline; returns true if the average is greater than 4.
+
+# Calculates average word length within headline; returns true if the average is greater than 4.
 def averagewordlength(headline):
     charactercount = 0;
     wordcount = 0;
-    commonshortwords = ["the", "a", "for", "an", "of", "and", "so", "but", "with", ",",".",":",";"]
+    commonshortwords = ["the", "a", "for", "an", "of", "and", "so", "but", "with", ",", ".", ":", ";"]
     for w in word_tokenize(headline):
         if w.lower() not in commonshortwords:
             charactercount += len(w)
             wordcount += 1
-    avg = charactercount/wordcount
+    avg = charactercount / wordcount
     return avg < 4
 
-#Checks for the most common POS tag in the headline; returns true if the most common tag is NN.
+
+# Checks for the most common POS tag in the headline; returns true if the most common tag is NN.
 def mostcommontag(headline):
-	tags = [w[1] for w in pos_tag(word_tokenize(headline))]
-	counts = Counter(tags)
-	return counts.most_common()[0][0] == "NN"
+    tags = [w[1] for w in pos_tag(word_tokenize(headline))]
+    counts = Counter(tags)
+    return counts.most_common()[0][0] == "NN"
 
-#Checks for the use of superlative adjectives in the headline; returns true if any found.
+
+# Checks for the use of superlative adjectives in the headline; returns true if any found.
 def superlative(headline):
-	tags = [w[1] for w in pos_tag(word_tokenize(headline))]
-	for tag in tags:
-		if tag == 'JJS' or tag == 'RBS':
-			return True
-	return False
+    tags = [w[1] for w in pos_tag(word_tokenize(headline))]
+    for tag in tags:
+        if tag == 'JJS' or tag == 'RBS':
+            return True
+    return False
 
-#Checks for the use of wh-words in the headline; returns true if any found.
+
+# Checks for the use of wh-words in the headline; returns true if any found.
 def wh(headline):
-	tags = [w[1] for w in pos_tag(word_tokenize(headline))]
-	for tag in tags:
-		if tag == 'WP':
-			return True
-	return False
+    tags = [w[1] for w in pos_tag(word_tokenize(headline))]
+    for tag in tags:
+        if tag == 'WP':
+            return True
+    return False
 
-#Checks whether the first word in a headline is tagged as a bare-form verb, indicating an imperative;
-#returns true if it is.
+
+# Checks whether the first word in a headline is tagged as a bare-form verb, indicating an imperative;
+# returns true if it is.
 def imperative(headline):
-	tags = [w[1] for w in pos_tag(word_tokenize(headline))]
-	if tags[0] == 'VB':
-		return True
-	return False
+    tags = [w[1] for w in pos_tag(word_tokenize(headline))]
+    if tags[0] == 'VB':
+        return True
+    return False
 
-#Checks all bigrams in the headline, and compares them against a list of most common clickbait bigrams. Returns
-#true if any match.
+
+# Checks all bigrams in the headline, and compares them against a list of most common clickbait bigrams. Returns
+# true if any match.
 def bigrams(headline):
-	bigrams = nltk.bigrams(headline.split(" "))
-	for x in bigrams:
-		if x in common_bigrams:
-			return True
-	return False
+    bigrams = nltk.bigrams(headline.split(" "))
+
+    for x in bigrams:
+        if x in common_bigrams:
+            return True
+    return False
+
 
 def train_classifier(training_set):
     classifier = nltk.NaiveBayesClassifier.train(training_set)
     return classifier
+
 
 def evaluate_classifier(classifier, test_set):
     print(nltk.classify.accuracy(classifier, test_set))
 
 
 if __name__ == '__main__':
-	
     labeled_data = create_labeled_data()
     training_set, test_set = create_feature_sets(labeled_data)
     classifier = train_classifier(training_set)
--- data_collector.py
+++ data_collector.py
@@ -6,37 +6,42 @@ Quick/dirty file that collects & pickles data
 import pickle
 import random
 from newsapi import NewsApiClient
+import nltk
+
+bait_headlines = []
+legit_headlines = []
 
 
 def create_labeled_data():
-    news_api = NewsApiClient(api_key='99a7e430ec8f4cc1b2e6a6b77b65a5bc')
-    buzzfeed_headlines = []
-    reuters_headlines = []
+    news_api = NewsApiClient(api_key='063f02817dbb49528058d7372964f645')
     x = 1
     while x <= 10:
-        b_headlines = news_api.get_everything(sources='buzzfeed',
-                                              from_param='2018-11-11',
-                                              to='2018-12-09',
-                                              language='en',
-                                              sort_by='relevancy',
-                                              page_size=100,
-                                              page=x)['articles']
-        r_headlines = news_api.get_everything(sources='reuters',
-                                              from_param='2018-11-11',
-                                              to='2018-12-09',
-                                              language='en',
-                                              sort_by='relevancy',
-                                              page_size=100,
-                                              page=x)['articles']
+        b_headlines = \
+            news_api.get_everything(sources='buzzfeed', from_param='2018-11-16', to='2018-12-15',
+                                    language='en',
+                                    sort_by='relevancy', page_size=100, page=x)['articles']
+        m_headlines = \
+            news_api.get_everything(sources='mtv-news', from_param='2018-11-16', to='2018-12-15',
+                                    language='en',
+                                    sort_by='relevancy', page_size=100, page=x)['articles']
+        r_headlines = \
+            news_api.get_everything(sources='reuters', from_param='2018-11-16', to='2018-12-15',
+                                    language='en', sort_by='relevancy', page_size=100, page=x)['articles']
+        a_headlines = \
+            news_api.get_everything(sources='associated-press', from_param='2018-11-16', to='2018-12-15',
+                                    language='en', sort_by='relevancy', page_size=100, page=x)['articles']
         b_titles = [article['title'] for article in b_headlines]
-        b_titles = list(filter(None.__ne__,b_titles))
+        b_titles = list(filter(None.__ne__, b_titles))
         b_titles = [(article, 'bait') for article in b_titles]
-        buzzfeed_headlines.extend(b_titles)
+        bait_headlines.extend(b_titles)
+        m_titles = [(article['title'], 'bait') for article in m_headlines]
+        bait_headlines.extend(m_titles)
         r_titles = [(article['title'], 'not_bait') for article in r_headlines]
-        reuters_headlines.extend(r_titles)
+        legit_headlines.extend(r_titles)
+        a_titles = [(article['title'], 'not_bait') for article in a_headlines]
+        legit_headlines.extend(a_titles)
         x += 1
-
-    all_headlines = buzzfeed_headlines + reuters_headlines
+    all_headlines = bait_headlines + legit_headlines
     random.shuffle(all_headlines)
     pickle.dump(all_headlines, open('headlines.p', 'wb'))
 
