a7a14e5ad5791d8bed3d492c3e84e22abe3db647
Samantha Richards <samantharichards@Klaus-the-Komputer.local>
2018-12-18 16:18:23 -0500

    Samantha's commit: added 2 features + oop version

--- clickbaitoop.py
+++ clickbaitoop.py
@@ -10,37 +10,33 @@ import random
 import pickle
 from collections import Counter
 from nltk.corpus import stopwords
+from headline import Headline
 
 common_bigrams = []
+splitpoint = 2700
+
 """
 Opens the pickled file and shuffles it up, returning a list of labeled headlines
 """
+def create_headlines():
+    headline_tuples = pickle.load(open('headlines.p', 'rb'))
+    headlines = [Headline(h[0], h[1]) for h in headline_tuples]
+    random.shuffle(headlines)
+    return headlines
 
-
-def create_labeled_data():
-    all_headlines = pickle.load(open('headlines.p', 'rb'))
-    random.shuffle(all_headlines)
-    return all_headlines
-
-
-def create_feature_sets(labeled_data):
-    common = get_bigrams(labeled_data)
-    common_bigrams.extend(common)
-    feature_sets = [(bait_features(headline), label) for (headline, label) in labeled_data]
+def create_feature_sets(headlines):
+    common_bigrams.extend(get_bigrams(headlines))
+    feature_sets = [(bait_features(headline), headline.label) for headline in headlines]
     print(len(feature_sets))
-    train_set, test_set = feature_sets[:2700], feature_sets[2700:]
+    train_set, test_set = feature_sets[:splitpoint], feature_sets[splitpoint:]
     return train_set, test_set
 
 
 #pulls out most frequent bigrams from the training set each time (they are almost always the same!)
-def get_bigrams(lst):
-    training = lst[:2700]
-    training = [w for w in training if w[1] == 'bait']
-    training = [w[0] for w in training]
-    raw = " ".join(training)
-    tokens = raw.split(" ")
-    bigrams = list(nltk.bigrams(tokens))
-    fdist = nltk.FreqDist(bigrams)
+def get_bigrams(headlines):
+    clean_string = [" ".join(h.tokens) for h in headlines[:splitpoint] if h.label == 'bait']
+    bgrams = nltk.bigrams(clean_string)
+    fdist = nltk.FreqDist(bgrams)
     most_common = fdist.most_common(25)
     common_bigrams = [x[0] for x in most_common]
     return common_bigrams
@@ -54,34 +50,36 @@ def bait_features(headline):
     featureset['mostcommontag'] = mostcommontag(headline)
     featureset['wh'] = wh(headline)
     featureset['startswithnum'] = startswithnum(headline)
+    featureset['superlative'] = superlative(headline)
     featureset['imperative'] = imperative(headline)
     featureset['bigrams'] = bigrams(headline)
     featureset['function_words'] = function_words(headline)
     featureset['flag_words'] = flag_words(headline)
     return featureset
 
+
 # Checks for the use of first and second-person pronouns in article headline; returns true if any found.
 def procount(headline):
     pronouns = ["we", "you", "i", "everyone", "us", "your", "our"]
-    for w in word_tokenize(headline):
-        if w.lower() in pronouns:
+    for w in headline.tokens_lower:
+        if w in pronouns:
             return True
     return False
 
 
 # Checks end-of-sentence punctuation count in headline; returns true if count is greater than 0.
 def punct(headline):
-    count = 0
-    punct = [".", "!", "?"]
-    for w in word_tokenize(headline):
+    punct = [".", "!", "?", ","]
+    found = False
+    for w in headline.tokens:
         if w in punct:
-            count += 1
-    return count > 0
+            found = True
+    return found
 
 
 # Checks whether the headline starts with a digit; returns true if so.
 def startswithnum(headline):
-    tags = [w[1] for w in pos_tag(word_tokenize(headline))]
+    tags = [w[1] for w in headline.pos_tagged]
     if tags[0] == 'CD':
         return True
     return False
@@ -92,8 +90,8 @@ def averagewordlength(headline):
     charactercount = 0
     wordcount = 0
     commonshortwords = ["the", "a", "for", "an", "of", "and", "so", "but", "with", ",", ".", ":", ";"]
-    for w in word_tokenize(headline):
-        if w.lower() not in commonshortwords:
+    for w in headline.tokens_lower:
+        if w not in commonshortwords:
             charactercount += len(w)
             wordcount += 1
     avg = charactercount / wordcount
@@ -102,15 +100,13 @@ def averagewordlength(headline):
 
 # Checks for the most common POS tag in the headline; returns true if the most common tag is NN.
 def mostcommontag(headline):
-    tags = [w[1] for w in pos_tag(word_tokenize(headline))]
-    counts = Counter(tags)
+    counts = Counter(headline.pos_tags)
     return counts.most_common()[0][0] == "NN"
 
 
 # Checks for the use of superlative adjectives in the headline; returns true if any found.
 def superlative(headline):
-    tags = [w[1] for w in pos_tag(word_tokenize(headline))]
-    for tag in tags:
+    for tag in headline.pos_tags:
         if tag == 'JJS' or tag == 'RBS':
             return True
     return False
@@ -118,8 +114,7 @@ def superlative(headline):
 
 # Checks for the use of wh-words in the headline; returns true if any found.
 def wh(headline):
-    tags = [w[1] for w in pos_tag(word_tokenize(headline))]
-    for tag in tags:
+    for tag in headline.pos_tags:
         if tag == 'WP':
             return True
     return False
@@ -128,8 +123,7 @@ def wh(headline):
 # Checks whether the first word in a headline is tagged as a bare-form verb, indicating an imperative;
 # returns true if it is.
 def imperative(headline):
-    tags = [w[1] for w in pos_tag(word_tokenize(headline))]
-    if tags[0] == 'VB':
+    if headline.pos_tags[0] == 'VB':
         return True
     return False
 
@@ -137,7 +131,7 @@ def imperative(headline):
 # Checks all bigrams in the headline, and compares them against a list of most common clickbait bigrams. Returns
 # true if any match.
 def bigrams(headline):
-    bigrams = nltk.bigrams(headline.split(" "))
+    bigrams = nltk.bigrams(headline.tokens)
 
     for x in bigrams:
         if x in common_bigrams:
@@ -146,15 +140,14 @@ def bigrams(headline):
 
 # Calculates proportion of word in headline that are stopwords or function words
 def function_words(headline):
-    fun_words = [w for w in [word.lower() for word in word_tokenize(headline)] if w in stopwords.words('english')]
-    #return len(fun_words)/len(word_tokenize(headline))
-    return True if (len(fun_words)/len(word_tokenize(headline)) == .5) else False
+    fun_words = [w for w in headline.tokens_lower if w in stopwords.words('english')]
+    #return len(fun_words)/headline.num_tokens
+    return True if ((len(fun_words)/headline.num_tokens) == .5) else False
 
 def flag_words(headline):
     flags = ['this', 'will', 'believe', 'surprise']
     found = False
-    for word in word_tokenize(headline):
-        word = word.lower()
+    for word in headline.tokens_lower:
         if word in flags:
             found = True
     return found
@@ -170,11 +163,10 @@ def evaluate_classifier(classifier, test_set):
 
 
 if __name__ == '__main__':
-    labeled_data = create_labeled_data()
-    training_set, test_set = create_feature_sets(labeled_data)
+
+
+    headlines = create_headlines()
+    training_set, test_set = create_feature_sets(headlines)
     classifier = train_classifier(training_set)
     evaluate_classifier(classifier, test_set)
-    classifier.show_most_informative_features(20)
-
-
-
+    classifier.show_most_informative_features()
