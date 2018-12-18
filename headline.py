b908b2fcbdd448bfc72e0163e452b37f92e23aa4
Samantha Richards <samantharichards@Klaus-the-Komputer.local>
2018-12-18 16:20:59 -0500

    forgot headline file

---
+++ headline.py
@@ -0,0 +1,14 @@
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+
+class Headline:
+    def __init__(self, string, label):
+        self.string = string
+        self.label = label
+        self.tokens = word_tokenize(string)
+        self.tokens_lower = [token.lower() for token in self.tokens]
+        self.num_tokens = len(self.tokens)
+        self.pos_tagged = pos_tag(self.tokens)
+        self.pos_tags = [tag[1] for tag in self.pos_tagged]
+
+
