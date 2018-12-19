45273aba6968cc16c6ee052edadcab1f1a94d604
chianobi <39969739+chianobi@users.noreply.github.com>
2018-12-19 13:54:36 -0500

    pickled trained classifier, added feature that lets user check clickbait content of news source

--- data_collector.py
+++ data_collector.py
@@ -17,30 +17,34 @@ def create_labeled_data():
     x = 1
     while x <= 10:
         b_headlines = \
-            news_api.get_everything(sources='buzzfeed', from_param='2018-11-16', to='2018-12-15',
+            news_api.get_everything(sources='buzzfeed', from_param='2018-11-17', to='2018-12-15',
                                     language='en',
                                     sort_by='relevancy', page_size=100, page=x)['articles']
         m_headlines = \
-            news_api.get_everything(sources='mtv-news', from_param='2018-11-16', to='2018-12-15',
+            news_api.get_everything(sources='mtv-news', from_param='2018-11-17', to='2018-12-15',
                                     language='en',
                                     sort_by='relevancy', page_size=100, page=x)['articles']
         r_headlines = \
-            news_api.get_everything(sources='reuters', from_param='2018-11-16', to='2018-12-15',
+            news_api.get_everything(sources='reuters', from_param='2018-11-17', to='2018-12-15',
                                     language='en', sort_by='relevancy', page_size=100, page=x)['articles']
         a_headlines = \
-            news_api.get_everything(sources='associated-press', from_param='2018-11-16', to='2018-12-15',
+            news_api.get_everything(sources='associated-press', from_param='2018-11-17', to='2018-12-15',
                                     language='en', sort_by='relevancy', page_size=100, page=x)['articles']
         b_titles = [article['title'] for article in b_headlines]
         b_titles = list(filter(None.__ne__, b_titles))
         b_titles = [(article, 'bait') for article in b_titles]
         bait_headlines.extend(b_titles)
         m_titles = [(article['title'], 'bait') for article in m_headlines]
+        print(len(m_titles))
         bait_headlines.extend(m_titles)
         r_titles = [(article['title'], 'not_bait') for article in r_headlines]
         legit_headlines.extend(r_titles)
         a_titles = [(article['title'], 'not_bait') for article in a_headlines]
         legit_headlines.extend(a_titles)
         x += 1
+
+    print(len(bait_headlines))
+    print(len(legit_headlines))
     all_headlines = bait_headlines + legit_headlines
     random.shuffle(all_headlines)
     pickle.dump(all_headlines, open('headlines.p', 'wb'))
